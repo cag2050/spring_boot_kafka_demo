d5c19e39ebf191c78271c7374abc1292e76a3e63
chen_anguo <cag2050@163.com>
2019-08-22 20:34:16 +0800

    project init

---
+++ src/main/java/com/cag2050/spring_boot_kafka_demo/SpringBootKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cag2050.spring_boot_kafka_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKafkaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKafkaDemoApplication.class, args);
+    }
+
+}
